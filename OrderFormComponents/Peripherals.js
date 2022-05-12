355241d676ac3976fa2956b31a1efd6915ae1448
Anthony Aruanna <24880946+C4vemann@users.noreply.github.com>
2022-05-12 08:09:17 -0400

    Add files via upload

--- OrderFormComponents/Peripherals.js
+++ OrderFormComponents/Peripherals.js
@@ -1,20 +1,48 @@
 class Peripherals{
 	constructor(x){
-		this.value = x;
-		this.text = new Text("Peripherals: " + x);
+		this.values = x;
+		console.log(this.values);
+		this.text = this.text();
+		//this.input = this.input();
 
-		this.element = this.init(this.text);
+		this.element = this.init(this.text/*,this.input*/);
 	}
 
-	init(text){
-		let main = document.createElement("div");
-		main.appendChild(text.element);
+	text(){
+		let main = document.createElement("p");
+		main.className = "order_text";
+		main.innerText = "Peripherals: ";
 		return main;
 	}
 
-	changeValue(val){
-		this.value = val;
-		this.text.changeText("Peripherals: " + val);
+	/*change(){
+		console.log(this.value);
+		console.log(this.values);
+		console.log(this.values.at(this.id));
+		this.values[this.id] = this.value;
 	}
+
+	input(){
+		let main = document.createElement("div");
+		for(let i = 0; i < this.values.length; i++){
+			let sub = document.createElement("input");
+			sub.id = i;
+			sub.className = "order_input";
+			sub.value = this.values[i];
+			sub.readOnly = false;
+			sub.addEventListener("change", this.change,false);
+			main.appendChild(sub);
+		}
+		return main;
+	}*/
+
+	init(text/*,input*/){
+		let main = document.createElement("div");
+		main.className = "order_content";
+		main.appendChild(text);
+/*		main.appendChild(input);
+*/		return main;
+	}
+
 
 }
