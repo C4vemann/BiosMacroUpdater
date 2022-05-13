82b5969fd2d3d2e62cd32e5faf60fb0eb9e17967
Anthony Aruanna <24880946+C4vemann@users.noreply.github.com>
2022-05-12 23:24:34 -0400

    Add files via upload

---
+++ MyRegex.js
@@ -0,0 +1,52 @@
+class MyRegex{
+	static orderNumberMatcher(str){
+		if(str.length == 7){
+			let pattern = /^[0-9]{7}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static itemNumberMatcher(str){
+		if(str.length == 7){
+			let pattern = /^[0-9]{7}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static serialNumberMatcher(str){
+		if(str.length == 7){
+			let pattern = /^(?=.*[A-Z])(?=.*\d)[A-Z\d]{7}$/;
+			return pattern.test(str);
+		} else if(str.length == 10){
+			let pattern = /^MXL(?=.*[A-Z])(?=.*\d)[A-Z\d]{7}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static assetNumberMatcher(str){
+		if(str.length == 10){
+			let pattern = /^ITE[0-9]{7}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static peripheralNumberMatcher(str){
+		if(str.length == 12){
+			let pattern = /^(?=.*[A-Z])(?=.*\d)[A-Z\d]{12}$/;
+			return pattern.test(str);
+		} else if(str.length == 15){
+			let pattern = /^(?=.*[A-Z])(?=.*\d)[A-Z\d]{15}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+}
