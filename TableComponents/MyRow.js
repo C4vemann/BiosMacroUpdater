7f39de863295aaba7c263e7fb06408cfae08b86b
Anthony Aruanna <24880946+C4vemann@users.noreply.github.com>
2022-06-01 19:46:29 -0700

    Add files via upload

--- TableComponents/MyRow.js
+++ TableComponents/MyRow.js
@@ -1,9 +1,11 @@
 class MyRow{
-	constructor(t,a){
-		this.parentTable = t;
-		this.x = new MyColumn(a.id);
-		this.y = new MyColumn(a.name);
-		this.z = new MyColumn(a.version);
+	constructor(t,id,a){
+		this.parentList = t;
+		this.id = id;
+		this.isEditing = false;
+		this.x = new MyIdColumn(a.id);
+		this.y = new MyNameColumn(a.name);
+		this.z = new MyVersionColumn(a.version);
 
 		this.element = this.init(this.x,this.y,this.z);
 	}
@@ -17,13 +19,44 @@ class MyRow{
 
 		let editButton = document.createElement("button");
 		editButton.innerText = "Edit";
-		editButton.addEventListener("click",()=>{console.log(this)},false);
+		editButton.addEventListener("click",()=>{
+			if(this.isEditing){
+				this.isEditing = false;
+				this.x.input.readOnly = true;
+				this.y.input.readOnly = true;
+				this.z.input.readOnly = true;
+				editButton.innerText = "Edit";
+			} else {
+				this.isEditing = true;
+				this.x.input.readOnly = false;
+				this.y.input.readOnly = false;
+				this.z.input.readOnly = false;
+				editButton.innerText = "Save";
+			}
+		},false);
 
 		main.appendChild(editButton);
 
 		let deleteButton = document.createElement("button");
 		deleteButton.innerText = "X";
-		deleteButton.addEventListener("click",()=>{console.log(this.parentTable)},false);
+		deleteButton.addEventListener("click",()=>{
+
+			if(this.parentList.top <= 0){
+				console.log("List too small");
+			} else{
+				this.element.remove();
+
+				for(let x = this.id; x < this.parentList.list.length; x++){
+					if(this.parentList.list[x+1] != null){
+						this.parentList.list[x] = this.parentList.list[x+1];
+						this.parentList.list[x].id = x;
+					}
+				}
+				this.parentList.pop();
+			}
+
+			console.log(this.parentList.list);
+		},false);
 		
 		main.appendChild(deleteButton);
 		
--- TableComponents/MyTable.js
+++ TableComponents/MyTable.js
@@ -1,9 +1,9 @@
 class MyTable{
 	constructor(data){
 		this.headers = new MyHeaderRow(this,['ID','Name','Version']);
-		this.rows = new Array();
+		this.rows = new MyRowList();
 		for(let d of data){
-			this.rows.push(new MyRow(this,d));
+			this.rows.push(d);
 		}
 		this.element = this.init(this.headers,this.rows);
 	}
@@ -13,10 +13,15 @@ class MyTable{
 
 		main.appendChild(headers.element);
 
-		for(let row of this.rows){
+		for(let row of this.rows.list){
 			main.appendChild(row.element);
 		}
 
 		return main;
+	}
+
+	add(x,y,z){
+		this.rows.push({id:x,name:y,version:z});
+		this.element.appendChild(this.rows.list[this.rows.list.length - 1].element);
 	}
 }
