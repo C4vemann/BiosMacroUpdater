6b445abdc222d65759f12aee955f79d91a37114c
Anthony Aruanna <24880946+C4vemann@users.noreply.github.com>
2022-07-02 12:46:54 -0400

    Add files via upload

---
+++ TestingFolder/MyRegex.js
@@ -0,0 +1,69 @@
+class MyRegex{
+	static orderNumberMatcher(str){
+		if(str.length == 6){
+			let pattern = /^[0-9]{6}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static itemNumberMatcher(str){
+		if(str.length == 7){
+			let pattern = /^[0-9]{7}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static serialNumberMatcher(str){
+		if(str.length == 7){
+			let pattern = /^(?=.*[A-Z])(?=.*\d)[A-Z\d]{7}$/;
+			return pattern.test(str);
+		} else if(str.length == 10){
+			let pattern = /^MXL(?=.*[A-Z])(?=.*\d)[A-Z\d]{7}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static assetNumberMatcher(str){
+		if(str.length == 11){
+			let pattern = /^ITE[0-9]{8}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static peripheralNumberMatcher(str){
+		if(str.length == 12){
+			let pattern = /^(?=.*[A-Z])(?=.*\d)[A-Z\d]{12}$/;
+			return pattern.test(str);
+		} else if(str.length == 15){
+			let pattern = /^(?=.*[A-Z])(?=.*\d)[A-Z\d]{15}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static versionNumberMatcher(str){
+		if(str.length >= 3 && str.length <= 15){
+			let pattern = /^(?:\d{1,})(?:\.(?:\d{1,})){1,}$/;
+			return pattern.test(str);
+		} else {
+			return false;
+		}
+	}
+
+	static nameMatcher(str){
+		if(str.length > 0 && str.length < 25){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
